37d56db9b03eca70ba768c75bbd558f92c84da20
sereypsv <valid_@tut.by>
2023-06-10 18:41:34 +0300

    finish exercice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,31 @@
-//--- Декларативный стиль ---//
 import logo from "./logo.svg";
 import "./App.css";
+// import { createElement } from "react";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
-	);
-}
+export const App = () => {
+	const appBlock = createNewElement("div", "App");
+	const appHeaderBlock = createNewElement("header", "App-header");
+	const appLogoBlock = createNewElement("img", "App-logo");
+	appLogoBlock.setAttribute("src", `${logo}`);
+	appLogoBlock.setAttribute("alt", "logo");
+	const logoLinck = document.createElement("p");
+	logoLinck.innerHTML =
+		"<p>Edit <code>src/App.js</code> and save to reload.</p>";
+	const appLinkBlock = createNewElement("a", "App-link");
+	appLinkBlock.setAttribute("href", "https://reactjs.org");
+	appLinkBlock.setAttribute("target", "_blank");
+	appLinkBlock.setAttribute("rel", "noopener noreferrer");
+	appLinkBlock.textContent = "Learn React";
+	const currentYear = createNewElement("p", "");
+	currentYear.textContent = `${new Date().getFullYear()}`;
+	appHeaderBlock.append(appLogoBlock, logoLinck, appLinkBlock, currentYear);
+	appBlock.append(appHeaderBlock);
+	console.log(appBlock);
+	return appBlock;
+};
 
-export default App;
-//--- нечего отнести к императивному стилю ---//
+const createNewElement = (element, className) => {
+	const createEl = document.createElement(element);
+	createEl.className = className;
+	return createEl;
+};
